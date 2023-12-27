933307d666809a9d3f94412697b92d2130a5b815
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2023-12-27 21:40:56 +0800

    todo assign done

--- dom/5_todoapp/script.js
+++ dom/5_todoapp/script.js
@@ -1,6 +1,13 @@
 let root = document.getElementById("root");
 let cards;
 
+let receiver = new Object({});
+let todoArr = new Array([]);
+let inprogArr = new Array([]);
+let stuckArr = new Array([]);
+let doneArr = new Array([]);
+// console.log({ receiver });
+
 function interface() {
   cards = document.createElement("div");
   let cardName = ["To do", "In progress", "Stuck", "Done"];
@@ -18,7 +25,7 @@ function interface() {
     headerDiv.className = "cardHeadDiv";
     header.className = "header";
     card.className = "card";
-    todoes.className = "todoes";
+    todoes.id = `todoes${i}`;
 
     header.innerText = `${cardName[i]}`;
     addButt.innerText = "+ Add card";
@@ -34,8 +41,9 @@ function interface() {
 }
 
 let task;
+let classnames;
 function makeTaskDiv() {
-  let classnames = ["title", "desc", "status", "priority"];
+  classnames = ["title", "desc", "status", "priority"];
   let innerText = ["Title", "Description", "Status", "Priority"];
 
   task = document.createElement("div");
@@ -58,6 +66,7 @@ function makeTaskDiv() {
   let addTaskButt = document.createElement("button");
   addTaskButt.innerText = "Add task";
   addTaskButt.setAttribute("class", "addTaskButt");
+  addTaskButt.addEventListener("click", addTaskButtF);
 
   task.appendChild(addTaskButt);
   root.appendChild(task);
@@ -67,10 +76,197 @@ function makeTaskDivshow() {
   task.style.display = "flex";
 }
 
+function todoesF() {
+  if (todoArr != "" && todoArr[0].Status == "todo") {
+    todoArr.map((e) => {
+      console.log("todoesF dotroos hewlew todoArr:", todoArr);
+      console.log(
+        "todoesF dotroos hewlew todoArr.Title:",
+        todoArr[todoArr.length - 1].Title
+      );
+      console.log(todoArr.Description);
+      console.log(todoArr.Status);
+      console.log(todoArr.Priority);
+      let todo = document.createElement("div");
+      let p1 = document.createElement("p");
+      let p2 = document.createElement("p");
+      let p3 = document.createElement("p");
+      let p4 = document.createElement("p");
+
+      p1.innerText = `${todoArr[todoArr.length - 1].Title}`;
+      p2.innerText = `${todoArr[todoArr.length - 1].Description}`;
+      p3.innerText = `${todoArr[todoArr.length - 1].Status}`;
+      p4.innerText = `${todoArr[todoArr.length - 1].Priority}`;
+
+      todo.appendChild(p1);
+      todo.appendChild(p2);
+      todo.appendChild(p3);
+      todo.appendChild(p4);
+
+      console.log({ p1 });
+      console.log({ p2 });
+      console.log({ p3 });
+      console.log({ p4 });
+      let todoes = document.getElementById("todoes0");
+      todoes.appendChild(todo);
+    });
+  }
+
+  if (inprogArr != "" && todoArr[0].Status == "in progress") {
+    inprogArr.map((e) => {
+      console.log("todoesF dotroos hewlew inprogArr:", inprogArr);
+      console.log(
+        "todoesF dotroos hewlew inprogArr.Title:",
+        inprogArr[inprogArr.length - 1].Title
+      );
+      console.log(inprogArr.Description);
+      console.log(inprogArr.Status);
+      console.log(inprogArr.Priority);
+      let todo = document.createElement("div");
+      let p1 = document.createElement("p");
+      let p2 = document.createElement("p");
+      let p3 = document.createElement("p");
+      let p4 = document.createElement("p");
+
+      p1.innerText = `${inprogArr[inprogArr.length - 1].Title}`;
+      p2.innerText = `${inprogArr[inprogArr.length - 1].Description}`;
+      p3.innerText = `${inprogArr[inprogArr.length - 1].Status}`;
+      p4.innerText = `${inprogArr[inprogArr.length - 1].Priority}`;
+
+      todo.appendChild(p1);
+      todo.appendChild(p2);
+      todo.appendChild(p3);
+      todo.appendChild(p4);
+
+      console.log({ p1 });
+      console.log({ p2 });
+      console.log({ p3 });
+      console.log({ p4 });
+
+      let todoes = document.getElementById("todoes1");
+      todoes.appendChild(todo);
+    });
+  }
+
+  if (stuckArr != "" && todoArr[0].Status == "stuck") {
+    stuckArr.map((e) => {
+      console.log("todoesF dotroos hewlew stuckArr:", stuckArr);
+      console.log(
+        "todoesF dotroos hewlew stuckArr.Title:",
+        stuckArr[stuckArr.length - 1].Title
+      );
+      console.log(stuckArr.Description);
+      console.log(stuckArr.Status);
+      console.log(stuckArr.Priority);
+      let todo = document.createElement("div");
+      let p1 = document.createElement("p");
+      let p2 = document.createElement("p");
+      let p3 = document.createElement("p");
+      let p4 = document.createElement("p");
+
+      p1.innerText = `${stuckArr[stuckArr.length - 1].Title}`;
+      p2.innerText = `${stuckArr[stuckArr.length - 1].Description}`;
+      p3.innerText = `${stuckArr[stuckArr.length - 1].Status}`;
+      p4.innerText = `${stuckArr[stuckArr.length - 1].Priority}`;
+
+      todo.appendChild(p1);
+      todo.appendChild(p2);
+      todo.appendChild(p3);
+      todo.appendChild(p4);
+
+      console.log({ p1 });
+      console.log({ p2 });
+      console.log({ p3 });
+      console.log({ p4 });
+
+      let todoes = document.getElementById("todoes2");
+      todoes.appendChild(todo);
+    });
+  }
+
+  if (doneArr != "" && todoArr[0].Status == "done") {
+    doneArr.map((e) => {
+      console.log("todoesF dotroos hewlew doneArr:", doneArr);
+      console.log(
+        "todoesF dotroos hewlew doneArr.Title:",
+        doneArr[doneArr.length - 1].Title
+      );
+      console.log(doneArr.Description);
+      console.log(doneArr.Status);
+      console.log(doneArr.Priority);
+      let todo = document.createElement("div");
+      let p1 = document.createElement("p");
+      let p2 = document.createElement("p");
+      let p3 = document.createElement("p");
+      let p4 = document.createElement("p");
+
+      p1.innerText = `${doneArr[doneArr.length - 1].Title}`;
+      p2.innerText = `${doneArr[doneArr.length - 1].Description}`;
+      p3.innerText = `${doneArr[doneArr.length - 1].Status}`;
+      p4.innerText = `${doneArr[doneArr.length - 1].Priority}`;
+
+      todo.appendChild(p1);
+      todo.appendChild(p2);
+      todo.appendChild(p3);
+      todo.appendChild(p4);
+
+      console.log({ p1 });
+      console.log({ p2 });
+      console.log({ p3 });
+      console.log({ p4 });
+
+      let todoes = document.getElementById("todoes1");
+      todoes.appendChild(todo);
+    });
+  }
+}
+
 function addTaskButtF() {
-  //status-aas ni hamaarch ali arra-ruu oroh we gedgiig ni shiidne.
-  //card bolgonii todo haragdaj baigaa hesgiig dawtalt ashiglaj shineer zurna.
-  //
+  //collect task's information into a object
+  //take criteria on status
+  //after 4 if condition move related one's array
+  //card bolgonii todo haragdaj baigaa hesgiig dawtalt ashiglaj shineer zurna
+  //erase all datas from add task and make them dissappear
+
+  for (let i = 0; i < 4; i++) {
+    let keyArr = ["Title", "Description", "Status", "Priority"];
+    console.log(
+      "barij awsan medeelel",
+      document.getElementById(classnames[i]).value
+    );
+    receiver[`${keyArr[i]}`] = document.getElementById(classnames[i]).value;
+    console.log("hiisen gazar ni", receiver);
+    console.log("receiver ", i, "=", receiver, typeof receiver, "    in for");
+  }
+
+  console.log("receiver ", "=", receiver, typeof receiver, "out of for");
+  console.log("receiver status:", receiver.Status, typeof receiver.Status);
+
+  if (receiver.Status == "todo") {
+    console.log("todo length", todoArr.length);
+    todoArr[todoArr.length - 1] = receiver;
+    console.log({ receiver });
+    console.log("todo-g addtask fn-ees hewlew:", todoArr);
+  } else if (receiver.Status == "in progress") {
+    console.log(
+      "uul ni status ni in progress mon baina. daanch tsaashaa unshixggui baina"
+    );
+    console.log("todo length", todoArr.length);
+    inprogArr[inprogArr.length - 1] = receiver;
+    console.log({ receiver });
+    console.log({ inprogArr });
+  } else if (receiver.Status == "stuck") {
+    console.log("todo length", todoArr.length);
+    stuckArr[stuckArr.length - 1] = receiver;
+    console.log({ receiver });
+    console.log({ stuckArr });
+  } else if (receiver.Status == "done") {
+    console.log("todo length", todoArr.length);
+    doneArr[doneArr.length - 1] = receiver;
+    console.log({ receiver });
+    console.log({ doneArr });
+  }
+  todoesF();
 }
 
 interface();
