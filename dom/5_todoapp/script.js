e63a6e7c9510fe4b9e54d1b76a5f93576fd5b632
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2023-12-27 12:21:40 +0800

    todo doing

--- dom/5_todoapp/script.js
+++ dom/5_todoapp/script.js
@@ -1,6 +1,71 @@
-// odoo ymarshuu baih we gedgiig bichiy
-// hamgiin ehend tsulgui tsagaan deer gantshan towch baina. daraad orhoor todo ni haragdna
-// todo ni card baidlaar baina. barun gar tald ni card nemeh towch baina
-// Card ni Header hesegtei. emojitoi. dooshoo checklist-uudtei darhaar bichig ni superscript boldog. hamgiin door n shine todo nemeh hesegtei. enter darhaar bas shine mor nemegddeg baih bolno.
+//sequence
+//  first apperance, contains 4 card (header, input, add button), whole background image
+//  new todo window, contains header = todo name, desc, priority, status
+//
+//
 
-//feature - tag ogch boldog baina. progress-oor ni hamaarch filterdeh bolomjtoi, sortloh, groupleh bolomjtoi.
+//cool features
+//
+//
+let root = document.getElementById("root");
+let cards = document.createElement("div");
+let cardName = ["To do", "In progress", "Stuck", "Done"];
+
+function interface() {
+  for (let i = 0; i < 4; i++) {
+    let card = document.createElement("div");
+    let headerDiv = document.createElement("div");
+    let header = document.createElement("h2");
+    let todoCount = document.createElement("p");
+    let todoes = document.createElement("div");
+    let addButt = document.createElement("button");
+
+    card.className = "card";
+    cards.className = "cards";
+    headerDiv.className = "cardHeadDiv";
+    header.className = "header";
+    card.className = "card";
+    todoes.className = "todoes";
+
+    header.innerText = `${cardName[i]}`;
+    addButt.innerText = "+ Add card";
+    addButt.addEventListener("click", addtodo);
+
+    headerDiv.appendChild(header);
+    headerDiv.appendChild(todoCount);
+    card.appendChild(headerDiv);
+    card.appendChild(todoes);
+    card.appendChild(addButt);
+    cards.appendChild(card);
+    root.appendChild(cards);
+  }
+}
+
+function addtodo() {
+  // todoes in to shown in card
+  // bainga baij l baidag window uusgene
+
+  let task = document.createElement("div");
+  let title = document.createElement("input");
+  let desc = document.createElement("input");
+  let status = document.createElement("input");
+  let priority = document.createElement("input");
+
+  title.className = "title";
+  desc.className = "desc";
+  status.className = "status";
+  priority.className = "priority";
+
+  title.setAttribute("name", "");
+  desc.className = "desc";
+  status.className = "status";
+  priority.className = "priority";
+
+  task.appendChild(title);
+  task.appendChild(desc);
+  task.appendChild(status);
+  task.appendChild(priority);
+  root.appendChild(task);
+}
+
+interface();
