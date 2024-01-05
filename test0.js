6acf1ec89358d8ab0bd06e1db23c38c3d1a5d870
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-01-05 09:37:24 +0800

    classwork

---
+++ test0.js
@@ -0,0 +1,13 @@
+// let obj = {};
+// function OnClickHandler(key, value) {
+//   obj = { [key]: value };
+//   console.log({ obj });
+//   return obj;
+// }
+
+// console.log(OnClickHandler("definedKey", "somevalue"));
+
+// Dot notation
+const obj = {};
+obj.propName = "Property value";
+console.log("obj:", obj);
